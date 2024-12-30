3b6cf050c6c00aa0ec63ed17f159375c56de1807
Misbah Ul Hasan <uxmisbahh@gmail.com>
2024-12-30 15:47:38 +0530

    changes three

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-                },
-              ],
-            },
-            reactStrictMode: true,
-          };
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  // output: 'export',
+
+  images: {
+        // loader: 'custom',
+        // loaderFile: './loader.js',
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
